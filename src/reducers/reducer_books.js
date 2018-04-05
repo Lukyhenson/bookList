8d048b6aa592e65c867a95d9f4ee060e479241fc
Tauan Tathiell <lukyhenson@live.com>
2018-04-05 11:42:29 -0300

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Harry Potter', pages: 101},
+        {title: 'Javascript the good parts', pages: 200},
+        {title: 'Vuejs is better?', pages: 150},
+        {title: 'Why angular?', pages: 190}
+    ]
+}
